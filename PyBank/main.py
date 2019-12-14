2f69949392d94880e1cfc7a51f6b7222f8dba6e1
cawthornebr <56853166+cawthornebr@users.noreply.github.com>
2019-12-14 16:02:47 -0700

    added total net amount

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,8 +5,14 @@ import csv
 #define path of csv file
 csvpath = os.path.join('Resources/budget_data.csv')
 
-#create a list for months
+#create a list for months and net profit/loss
 month=[]
+
+#total amout set as 0
+netamount=0
+
+#average change as list
+ave=[]
 
 #Improved Reading using CSV module
 with open(csvpath, newline='') as csvfile:
@@ -14,11 +20,13 @@ with open(csvpath, newline='') as csvfile:
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    #splits file by row
+    #skip first row of headers
+    header = next(csvreader)
+
+    #parse out rows
     for row in csvreader:
-
-        #add each month to the month list
         month.append(row[0])
+        netamount=netamount+int(row[1])
 
-#display the number of months minus the header
-print(len(month)-1)
+    print(len(month))
+    print(netamount)
