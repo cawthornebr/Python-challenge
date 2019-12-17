0a3791dfe9ebb3b13e6d572756bd15767f06fef9
cawthornebr <56853166+cawthornebr@users.noreply.github.com>
2019-12-17 09:20:54 -0700

    added write to text file for both poll and bank

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,7 +5,10 @@ import csv
 #define path of csv file
 csvpath = os.path.join('Resources/budget_data.csv')
 
-#create variables and lists
+# Specify the file to write to
+output_path = os.path.join("Results.txt")
+
+#create variables and lists needed
 month=[]
 amount=[]
 change=0
@@ -26,7 +29,7 @@ with open(csvpath, newline='') as csvfile:
     #skip first row of headers
     header = next(csvreader)
 
-    #parse out rows
+    #loop through csv
     for row in csvreader:
 
         #add to month count
@@ -34,6 +37,12 @@ with open(csvpath, newline='') as csvfile:
 
         #find out the change for current row
         change=int(row[1])-int(prev)
+
+        #reset prev row to current row for next loop
+        prev=row[1]
+
+        #add each profit and lot to total
+        netamount+=int(row[1])
 
         #find greatest increase amount and month
         if change>incamount:
@@ -44,25 +53,29 @@ with open(csvpath, newline='') as csvfile:
         if change<decamount:
             decamount=change
             decmonth=row[0]
-        
-        #reset prev row to current row for next loop
-        prev=row[1]
 
         #find the average change
         if ave == 0:
             ave=int(row[1])
         else:
-            ave = ave - int(row[1])
+            ave -= int(row[1])
             amount.append(ave)
             ave=int(row[1])
 
-        #add each profit and lot to total
-        netamount=netamount+int(row[1])
+# Open the file using "write" mode. Specify the variable to hold the contents
+with open(output_path, 'w', newline='') as txtfile:
 
-print("Financial Analysis")
-print("----------------------------")
-print(f'Total Months: {len(month)}')
-print(f'Total: ${netamount}')
-print(f'Average  Change: ${round(sum(amount)*-1/len(amount),2)}')
-print(f'Greatest Increase in Profits: {incmonth} ({incamount})')
-print(f'Greatest Decrease in Profits: {decmonth} ({decamount})')
+    # Initialize csv.writer
+    txtwriter = csv.writer(txtfile, delimiter=',', quoting=csv.QUOTE_NONE, quotechar='')
+
+    #print final text and write to file
+    print(f"Financial Analysis\n----------------------------\nTotal Months: {len(month)}\nTotal: ${netamount}")
+    print(f"Average  Change: ${round(sum(amount)*-1/len(amount),2)}\nGreatest Increase in Profits: {incmonth} (${incamount})")
+    print(f"Greatest Decrease in Profits: {decmonth} (${decamount})")
+    txtwriter.writerow(["Financial Analysis"])
+    txtwriter.writerow(["-------------------------"])
+    txtwriter.writerow(["Total Months: "+str(len(month))])
+    txtwriter.writerow(["Total: $"+str(netamount)])
+    txtwriter.writerow(["Average  Change: $"+str(round(sum(amount)*-1/len(amount),2))])
+    txtwriter.writerow(["Greatest Increase in Profits: "+incmonth+" ($"+str(incamount)+")"])
+    txtwriter.writerow(["Greatest Decrease in Profits: "+decmonth+" ($"+str(decamount)+")"])
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,10 +5,14 @@ import csv
 #define path of csv file
 csvpath = os.path.join('Resources/election_data.csv')
 
-votes={}
+# Specify the file to write to
+output_path = os.path.join("Results.txt")
 
-#create variables, lists, and dictionarys 
+#create variables, dictionaries, and lists needed
 totalvotes=0
+votes={}
+max=0
+maxname=""
 
 #Improved Reading using CSV module
 with open(csvpath, newline='') as csvfile:
@@ -19,27 +23,50 @@ with open(csvpath, newline='') as csvfile:
     #skip first row of headers
     header = next(csvreader)
     
+    #loop through csv
     for row in csvreader:
+
+        #assign variable vote to the name of the person in current row
         vote=row[2]
+
+        #if the name is in the dictionary, then add one. if it's not in dictionary, value for that name is 1
         if vote in votes:
             votes[vote] += 1
         else:
             votes[vote] = 1 
-        #for each row add one to the total votes
+        
+        #for each row add one to the total vote count
         totalvotes+=1
 
-max=0
-maxname=""
+# Open the file using "write" mode. Specify the variable to hold the contents
+with open(output_path, 'w', newline='') as txtfile:
 
-print(f"""Election Results
--------------------------
-Total Votes: {totalvotes}
--------------------------""")
-for vote, num in votes.items():
-    print(f'{vote}: {round(float(num)/totalvotes*100,3)}00% {str(num)}')
-    if num>max:
-        max=num
-        maxname=vote
-print(f"""-------------------------
-Winner: {maxname}
--------------------------""")
+    # Initialize csv.writer
+    txtwriter = csv.writer(txtfile, delimiter=',', quoting=csv.QUOTE_NONE, quotechar='')
+
+    #print first set of text and write to txt file
+    #--------------------
+    print(f"Election Results\n-------------------------\nTotal Votes: {totalvotes}\n-------------------------")
+    txtwriter.writerow(["Election Results"])
+    txtwriter.writerow(["-------------------------"])
+    txtwriter.writerow(["Total Votes: "+str(totalvotes)])
+    txtwriter.writerow(["-------------------------"])
+    #--------------------
+
+    #loop through dictionary
+    for vote, num in votes.items():
+
+        #print name, percent of total votes, and vote count for each name and write to txt file
+        print(f'{vote}: {round(float(num)/totalvotes*100,3)}00% ({str(num)})')
+        txtwriter.writerow([vote+": "+str(round(float(num)/totalvotes*100,3))+"00% ("+str(num)+")"])
+
+        #find max value and max value name
+        if num>max:
+            max=num
+            maxname=vote
+
+    #print the last section of text and write to txt file
+    print(f"-------------------------\nWinner: {maxname}\n-------------------------")
+    txtwriter.writerow(["-------------------------"])
+    txtwriter.writerow(["Winner: "+maxname])
+    txtwriter.writerow(["-------------------------"])
